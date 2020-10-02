938606b95c28312d27e8cea63eb0b8e39b423c52
Chandima Ranaweera <chan4lk@gmail.com>
2020-10-02 20:51:06 +0530

    add view pager

--- src/app/diet/home/home.page.ts
+++ src/app/diet/home/home.page.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  slidesOptions = {
+    spaceBetween: 8,
+    slidesPerView: 1,
+    freeMode: false,
+    loop: false,
+  };
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
