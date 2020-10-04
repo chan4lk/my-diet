f4429cccc893a34726fbdf3c9fc789995629afba
Chandima Ranaweera <chan4lk@gmail.com>
2020-10-04 20:37:31 +0530

    profile first slide

--- src/app/config/profile/profile.page.ts
+++ src/app/config/profile/profile.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +12,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
+  constructor(private fb: FormBuilder) {}
+  form: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  get gender() {
+    return this.form.get('gender').value;
   }
 
+  set gender(ev: CustomEvent) {
+    this.form.get('gender').patchValue(ev.detail.value);
+  }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      height: new FormControl('', Validators.required),
+      weight: new FormControl('', Validators.required),
+      age: new FormControl('', Validators.required),
+      gender: new FormControl('0', Validators.required),
+    });
+  }
+
+  next() {}
 }
