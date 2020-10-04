1cf9eefa96607ab89a05b4b723a75f5f85bf54b6
Chandima Ranaweera <chan4lk@gmail.com>
2020-10-04 22:22:53 +0530

    profile ui fixed

--- src/app/config/profile/profile.page.ts
+++ src/app/config/profile/profile.page.ts
@@ -16,13 +16,8 @@ export class ProfilePage implements OnInit {
   constructor(private fb: FormBuilder) {}
   form: FormGroup;
   targetForm: FormGroup;
-  get gender() {
-    return this.form.get('gender').value;
-  }
-
-  set gender(ev: CustomEvent) {
-    this.form.get('gender').patchValue(ev.detail.value);
-  }
+  goalForm: FormGroup;
+  activityForm: FormGroup;
 
   ngOnInit() {
     this.form = this.fb.group({
@@ -31,14 +26,33 @@ export class ProfilePage implements OnInit {
       age: new FormControl('', Validators.required),
       gender: new FormControl('0', Validators.required),
     });
+    this.activityForm = this.fb.group({
+      activityLevel: new FormControl('0', Validators.required),
+    });
 
     this.targetForm = this.fb.group({
-      target: new FormControl('', Validators.required),
+      target: new FormControl('0', Validators.required),
+    });
+    this.goalForm = this.fb.group({
+      goal: new FormControl('0', []),
+      weight: new FormControl('0', [Validators.required, Validators.min(1)]),
+    });
+
+    this.targetForm.valueChanges.subscribe((values) => {
+      if (values.target !== '1') {
+        this.goalForm.controls.weight.setValidators([
+          Validators.required,
+          Validators.min(1),
+        ]);
+      } else {
+        this.goalForm.controls.weight.clearValidators();
+      }
     });
   }
 
   next(slides: IonSlides) {
     slides.slideNext();
   }
+
   save() {}
 }
