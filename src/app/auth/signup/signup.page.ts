84f169628a85cd123a6bd4131d06bab29c569b30
Chandima Ranaweera <chan4lk@gmail.com>
2020-10-04 19:50:56 +0530

    signup api hooked

--- src/app/auth/signup/signup.page.ts
+++ src/app/auth/signup/signup.page.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  Form,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +16,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
-
-  constructor() { }
-
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router
+  ) {}
+  form: FormGroup;
   ngOnInit() {
+    this.form = this.fb.group(
+      {
+        firstName: new FormControl('', Validators.required),
+        lastName: new FormControl('', Validators.required),
+        email: new FormControl(
+          '',
+          Validators.compose([Validators.email, Validators.required])
+        ),
+        password: new FormControl(
+          '',
+          Validators.compose([Validators.minLength(6), Validators.required])
+        ),
+        confirmPassword: new FormControl(
+          '',
+          Validators.compose([Validators.minLength(6), Validators.required])
+        ),
+      },
+      { validators: this.passwordConfirming }
+    );
   }
 
+  passwordConfirming(c: AbstractControl): { invalid: boolean } {
+    if (c.get('password').value !== c.get('confirmPassword').value) {
+      return { invalid: true };
+    }
+  }
+
+  register() {
+    const value = this.form.value;
+
+    this.auth
+      .signup({
+        id: 0,
+        email: value.email,
+        name: value.firstName,
+        surname: value.lastName,
+        auth: {
+          login: value.email,
+          password: value.password,
+          roles: 1,
+        },
+      })
+      .subscribe((token) => this.router.navigate(['/profile']));
+  }
 }
